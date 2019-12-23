a3d76330676231eae3dcfe18e68af407b50a6994
renxingyu <renxingyu@hualala.com>
2019-12-23 15:24:07 +0800

    新增message组件

--- src/components/message/message.js
+++ src/components/message/message.js
@@ -1,15 +1,130 @@
-import React from 'react'
-import { Modal } from '../'
+import React, { useState, useEffect } from 'react'
+import ReactDOM from 'react-dom'
+import { Transition, TransitionGroup } from 'react-transition-group';
+import './message.css'
+import { getUid } from '../utils'
 
-export default function message(...config) {
+class Messages {
 
+  constructor() {
+    this.messagesList = []
+  }
+
+  findIndex = (obj) => {
+    return this.messagesList.findIndex((v) => v === obj)
+  }
+
+  close = (props, duration) => {
+    if (duration === 0) {
+      return
+    }
+    setTimeout(() => {
+      const index = this.findIndex(props)
+      this.messagesList[index].visible = false
+      this.render()
+    }, duration * 1000);
+  }
+
+  unMountMessage(props) {
+    const index = this.findIndex(props)
+    this.messagesList.splice(index, 1)
+    this.render()
+  }
+
+  push(content, duration, config = {}) {
+    let props = {
+      content: content,
+      visible: true,
+      animationOnEntered: () => {
+        this.close(props, duration)
+      },
+      animationOnExited: () => {
+        this.unMountMessage(props)
+      },
+      key: getUid(),
+      ...config
+    }
+    this.messagesList.push(props)
+    this.render()
+  }
+
+  info(...arge) {
+    this.push(...arge)
+  }
+
+  render() {
+    if (!this.div) {
+      this.div = document.createElement('div')
+      document.body.appendChild(this.div)
+    }
+
+    ReactDOM.render(
+      <div className='message'>
+        {this.messagesList.map((props) => (
+          <Message
+            {...props}
+          />
+        ))}
+      </div>
+      ,
+      this.div)
+  }
 }
 
+export default new Messages()
 
 function Message(props) {
-  return (
-    <div className='message-container'>
 
-    </div>
+  const [visible, setVisible] = useState(false)
+  useEffect(() => {
+    window.requestAnimationFrame(() => {
+      window.requestAnimationFrame(() => {
+        setVisible(props.visible)
+      })
+    })
+  }, [props.visible]);
+
+  const duration = 400;
+
+  const defaultStyle = {
+    // transform: 'translateY(-40%)',
+  }
+
+  const transitionStyles = {
+    entering: {
+      transition: `transform ${duration}ms ease`,
+      transform: 'translateY(0)',
+      position: 'relative',
+      zIndex: -1,
+    },
+    entered: {
+      transform: 'translateY(0)',
+    },
+    exiting: {
+      transition: `transform ${duration}ms ease`,
+      transform: 'translateY(-100%)'
+    },
+    exited: { transform: 'translateY(-40%)' },
+  };
+
+  return (
+    <Transition
+      in={visible}
+      timeout={duration}
+      onEntered={props.animationOnEntered}
+      onExited={props.animationOnExited}
+    >
+      {state => (
+        <div className='meesage-container'
+          style={{
+            ...defaultStyle,
+            ...transitionStyles[state]
+          }}>
+          <div className='message-content'>
+            {props.content}
+          </div>
+        </div>
+      )}
+    </Transition>
   )
 }
